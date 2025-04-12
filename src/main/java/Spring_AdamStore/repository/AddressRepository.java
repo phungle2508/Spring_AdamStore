d662b13f5b84c0ab7cfc255531b9087f7a4b343d
lean270804@gmail.com <lean270804@gmail.com>
2025-04-13 00:28:30 +0700

    update

--- src/main/java/Spring_AdamStore/repository/AddressRepository.java
+++ src/main/java/Spring_AdamStore/repository/AddressRepository.java
@@ -2,11 +2,13 @@ package Spring_AdamStore.repository;
 
 import Spring_AdamStore.entity.Address;
 import Spring_AdamStore.entity.Branch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
-
+    Page<Address> findAllByUserId(Long userId, Pageable pageable);
 }
